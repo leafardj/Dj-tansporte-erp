b0369c7659419397a3d4a127a9ce409a86d7a914
leafardj <Leafardj.88@gmail.com>
2026-07-09 19:27:53 -0300

    refactor: Simplify Motorista model to match specifications

--- lib/models/motorista.dart
+++ lib/models/motorista.dart
@@ -1,26 +1,18 @@
 class Motorista {
   final int? id;
   final String nome;
-  final String telefone;
-  final String email;
-  final String cnh;
   final String cpf;
-  final String endereco;
-  final DateTime dataNascimento;
-  final DateTime dataAdmissao;
-  final bool ativo;
+  final String cnh;
+  final String categoria;
+  final String telefone;
 
   Motorista({
     this.id,
     required this.nome,
-    required this.telefone,
-    required this.email,
-    required this.cnh,
     required this.cpf,
-    required this.endereco,
-    required this.dataNascimento,
-    required this.dataAdmissao,
-    this.ativo = true,
+    required this.cnh,
+    required this.categoria,
+    required this.telefone,
   });
 
   // Converter para Map para banco de dados
@@ -28,14 +20,10 @@ class Motorista {
     return {
       'id': id,
       'nome': nome,
-      'telefone': telefone,
-      'email': email,
-      'cnh': cnh,
       'cpf': cpf,
-      'endereco': endereco,
-      'dataNascimento': dataNascimento.toIso8601String(),
-      'dataAdmissao': dataAdmissao.toIso8601String(),
-      'ativo': ativo ? 1 : 0,
+      'cnh': cnh,
+      'categoria': categoria,
+      'telefone': telefone,
     };
   }
 
@@ -44,14 +32,10 @@ class Motorista {
     return Motorista(
       id: map['id'],
       nome: map['nome'],
-      telefone: map['telefone'],
-      email: map['email'],
-      cnh: map['cnh'],
       cpf: map['cpf'],
-      endereco: map['endereco'],
-      dataNascimento: DateTime.parse(map['dataNascimento']),
-      dataAdmissao: DateTime.parse(map['dataAdmissao']),
-      ativo: map['ativo'] == 1,
+      cnh: map['cnh'],
+      categoria: map['categoria'],
+      telefone: map['telefone'],
     );
   }
 
@@ -59,26 +43,18 @@ class Motorista {
   Motorista copyWith({
     int? id,
     String? nome,
-    String? telefone,
-    String? email,
-    String? cnh,
     String? cpf,
-    String? endereco,
-    DateTime? dataNascimento,
-    DateTime? dataAdmissao,
-    bool? ativo,
+    String? cnh,
+    String? categoria,
+    String? telefone,
   }) {
     return Motorista(
       id: id ?? this.id,
       nome: nome ?? this.nome,
-      telefone: telefone ?? this.telefone,
-      email: email ?? this.email,
-      cnh: cnh ?? this.cnh,
       cpf: cpf ?? this.cpf,
-      endereco: endereco ?? this.endereco,
-      dataNascimento: dataNascimento ?? this.dataNascimento,
-      dataAdmissao: dataAdmissao ?? this.dataAdmissao,
-      ativo: ativo ?? this.ativo,
+      cnh: cnh ?? this.cnh,
+      categoria: categoria ?? this.categoria,
+      telefone: telefone ?? this.telefone,
     );
   }
 }
